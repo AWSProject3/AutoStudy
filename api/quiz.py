de3449fc7485c244c61b4b7c9c0cbe300fa9d497
JunYoung_Kim <hjklo0220@gmail.com>
2024-07-08 14:53:21 +0900

    add grade quiz api

--- api/quiz.py
+++ api/quiz.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from models.repository import QuizRepository
 
-from schemas.quiz.request import GenerateQuizRequest
+from schemas.quiz.request import GenerateQuizRequest, GradeQuizRequest
 from schemas.quiz.response import GenerateQuizResponse, GradeQuizResponse
 from services.quiz_service import QuizService
 
@@ -12,14 +12,13 @@ def generate_quiz_handler(
     request: GenerateQuizRequest,
     quiz_repo: QuizRepository = Depends(),
 ) -> GenerateQuizResponse:
-    
-    response = QuizService(request, quiz_repo)
-    return response.create_quiz()
+    return QuizService(request, quiz_repo).create_quiz()
 
 @router.post("/grade", status_code=201)
 def grading_quiz_handler(
-    request: GenerateQuizRequest,
+    request: GradeQuizRequest,
+    quiz_repo: QuizRepository = Depends(),
 ) -> GradeQuizResponse:
 
-    response = QuizService(request)
+    response = QuizService(request, quiz_repo)
     return response.grade_quiz()
